b738b8ac032994f33e587df37b3f4bd56e5b791c
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-06-15 12:29:53 +0200

    Add a guess game in Rust

---
+++ Rust/guess.rs
@@ -0,0 +1,31 @@
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = 12;
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("You guessed {}.", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
